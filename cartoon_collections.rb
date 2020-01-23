0f67aa551dc98573a05825d8689da4a9b77980cf
Erica Schimmel <ericaschimmel01@gmail.com>
2020-01-23 20:50:57 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,13 +1,29 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves(dwarf_list) # code an argument here
   # Your code here
+  
+  dwarf_list.each_with_index do |dwarf, index|
+    puts "#{index+1}. #{dwarf}"
+  end
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(planeteer_call) # code an argument here
   # Your code here
+  
+  return planeteer_call.collect do |call|
+    call = call.capitalize + "!"
+  end
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(planeteer_call) # code an argument here
   # Your code here
+  
+  planeteer_call.each do |call|
+    if call.length > 4
+      return false
+    end
+  end
+  
+  return true
 end
 
 def find_the_cheese# code an argument here
