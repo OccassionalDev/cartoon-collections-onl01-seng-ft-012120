f7ffbbecfac141b5ce0fe61041d8df68150bfa13
Erica Schimmel <ericaschimmel01@gmail.com>
2020-01-23 20:55:03 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -26,7 +26,17 @@ def long_planeteer_calls(planeteer_call) # code an argument here
   return false
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(food_type) # code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  
+  cheese_types.each do |cheese|
+    food_type.each do |food|
+      if food == cheese
+        return cheese
+      end
+    end
+  end
+  
+  return nil
 end
